aa6641fc1311f8339567d8b0e8d6bbc259eb19d3
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-12-10 16:05:00 +0530

    Nvim Update

--- .config/nvim/lua/falcon/core/keymaps.lua
+++ .config/nvim/lua/falcon/core/keymaps.lua
@@ -1,11 +1,33 @@
 -- set leader key to space
 vim.g.mapleader = " "
 
+-- General Keymaps -------------------
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs and whatnot
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.cmd [[ set noswapfile ]]
+vim.cmd [[ set termguicolors ]]
+
+--Line numbers
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
 local keymap = vim.keymap -- for conciseness
 
 ---------------------
--- General Keymaps -------------------
-
 
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
